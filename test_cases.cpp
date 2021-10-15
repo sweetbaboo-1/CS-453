495f3753543732820666cbd8fcc9f07fcfbf15e7
Tyler Craig <tylercraigc4@gmail.com>
2021-10-15 17:27:13 -0600

    gave an example test case

--- test_cases.cpp
+++ test_cases.cpp
@@ -1,11 +1,20 @@
 #pragma once
 #include <iostream>
+#include "homograph.cpp"
+using namespace std;
+string PATH = "/home/user/cse453/secret/password.txt";
 
 // Non Homographs
-
+string nonHomograph1 = "home/user/cse453/secret/password.txt";
+string nonHomograph2 = "./secret/password.txt";
+string nonHomograph3 = "../password.txt";
 // Homographs
-
+string homograph1 = "/home/user/cse453/secret/password.txt";
+string homograph2 = "/Home/user/cse453/secret/password.txt";
+string homograph3 = "./../secret/password.txt";
 // runs through all defined test cases to ensure the program is functioning correctly
 void testCases() {
-    std::cout << "Running test cases...\n";
+    cout << "Running test cases...\n";
+    cout << "------ Non-homographs ------" << endl;
+    cout << PATH << " and " << nonHomograph1 << " : " << areHomographs(PATH, nonHomograph1) << endl;
 }
