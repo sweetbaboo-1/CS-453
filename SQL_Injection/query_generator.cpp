36889c473e6c1aa107966ed3ea6b1ab76e36178f
sweetbaboo-1 <72481120+sweetbaboo-1@users.noreply.github.com>
2021-10-19 03:23:56 -0400

    Update query_generator.cpp

--- SQL_Injection/query_generator.cpp
+++ SQL_Injection/query_generator.cpp
@@ -2,10 +2,15 @@
 
 #include <iomanip>
 
+/******************************************************
+ * TODO: figure out how this should work and code it
+ * THIS IS NOT DONE
+ *****************************************************/
 void generateQuerys(TestCase (&testCases)[6], std::string (&queries)[6]) 
 {
     for (int i = 0; i < 6; i++)
     {
+        // returning something to verify that it works as intended
         queries[i] = testCases[i].username + " " + testCases[i].password;
     }
 }
