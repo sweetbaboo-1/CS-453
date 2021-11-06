13f210d6c351c3dab7a85caaf79e610cc0c6e66b
dreyvonn <38699352+dreyvonn@users.noreply.github.com>
2021-11-05 18:11:16 -0600

    set up vulnerabilities with exploits with main function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "Homograph/homograph_main.cpp"
 #include "SQL_Injection/sql_main.cpp"
-#include "Memory_Organization/memory_organization_main.cpp"
+// #include "Memory_Organization/memory_organization_main.cpp"
+#include "Vulnerabilities_Exploits/vulnerabilitiesExploitsMain.cpp"
 int main()
 {
     bool keepRunning = true;
     std::string userInput = "";
-    
+
     while (keepRunning)
     {
-        std::cout << "\n1. Homograph Lab\n2. SQL Injection Lab\n3. Memory Lab\nQ. Exit\n";
+        std::cout << "\n1. Homograph Lab\n2. SQL Injection Lab\n3. Memory Lab\n4. Vulnerabilites and Exploits Lab\nQ. Exit\n";
         std::cin >> userInput;
 
         if (userInput == "1") // Homograph Lab
@@ -16,7 +17,10 @@ int main()
         else if (userInput == "2") // SQL Injection Lab
             sql();
         else if (userInput == "3")
-            memory_main();
+            // memory_main();
+            cout << "commented out because not working" << endl;
+        else if (userInput == "4")
+            vulnerabilitiesExploitsMain();
         else if (userInput == "q" || userInput == "Q") // exit
         {
             std::cout << "Thank you, goodbye\n";
