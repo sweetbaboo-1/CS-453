b902e37d7885857733abaa9faef9ff1e1233c625
Tyler Craig <tylercraigc4@gmail.com>
2021-12-11 13:59:53 -0700

    added my name to things

--- Encryption/cipher02.h
+++ Encryption/cipher02.h
@@ -12,10 +12,10 @@
 class Cipher02 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+   virtual std::string getPseudoAuth()  { return "Tyler Craig"; }
+   virtual std::string getCipherName()  { return "Hill Cipher"; }
+   virtual std::string getEncryptAuth() { return "Tyler Craig"; }
+   virtual std::string getDecryptAuth() { return "Tyler Craig"; }
 
    /***********************************************************
     * GET CIPHER CITATION
